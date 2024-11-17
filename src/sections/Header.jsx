d5c7da0492a7bcd1c5633c371316209f19bb501d
Jeevan George Antony <antony.jeevangeorge@gmail.com>
2024-11-17 19:06:37 +0530

    Enhance Header component with scroll detection and dynamic styling

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,22 +1,53 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link as LinkScroll } from "react-scroll";
 import clsx from "clsx";
 
-const NavLink = ({ title }) => (
-  <LinkScroll className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5">
-    {title}
-  </LinkScroll>
-);
-
 const Header = () => {
+  const [hasScrolled, setHasScrolled] = useState(false);
   const [isOpen, setIspen] = useState(false);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasScrolled(window.scrollY > 32);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const NavLink = ({ title }) => (
+    <LinkScroll
+      onClick={() => setIspen(false)}
+      to={title}
+      offset={-100}
+      spy
+      smooth
+      activeClass="nav-active"
+      className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5"
+    >
+      {title}
+    </LinkScroll>
+  );
+
   return (
-    <header className="fixed top-0 left-0 z-50 w-full py-10">
+    <header
+      className={clsx(
+        "fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4",
+        hasScrolled && "py-2 bg-black-100 backdrop-blur[8px]"
+      )}
+    >
       <div className="container flex h-14 max-lg:px-5">
-        <a className="lg:hidden flex-1 cursor-pointer z-2">
+        <LinkScroll
+          onClick={() => setIspen(false)}
+          to="hero"
+          offset={-250}
+          spy
+          smooth
+          className={clsx(
+            "lg:hidden flex-1 z-2 transition-transform duration-500 cursor-pointer"
+          )}
+        >
           <img src="/images/xora.svg" width={115} height={55} alt="logo" />
-        </a>
+        </LinkScroll>
         <div
           className={clsx(
             "w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0",
@@ -27,14 +58,14 @@ const Header = () => {
             <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
               <ul className="flex max-lg:block max-lg:px-12">
                 <li className="nav-li">
-                  <NavLink title={"Features"} />
+                  <NavLink title={"features"} />
                   <div className="dot" />
                   <NavLink title={"Pricing"} />
                 </li>
                 <li className="nav-logo">
                   <LinkScroll
                     to="hero"
-                    offset={-100}
+                    offset={-250}
                     spy
                     smooth
                     className={clsx(
